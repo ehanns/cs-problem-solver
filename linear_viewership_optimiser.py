3a8536beb54ca6acc8c2755f4a8164c169d5aff3
Hanns <ellen.hanns@sky.uk>
2017-10-17 15:14:04 +0100

    added in watershed constraint. calculates best plan now based on program weightings, grade weighting and maximises on viewership

--- linear_viewership_optimiser.py
+++ linear_viewership_optimiser.py
@@ -5,7 +5,7 @@ from programme_grade_limits import programme_grade_limits
 from operator import itemgetter
 import calendar
 
-#OBJECTIVE = MAXIMISE VIEWERSHIP FOR A GIVEN PROGRAMME K OF QUALITY X ON SLOT I ON DAY J
+# OBJECTIVE = MAXIMISE VIEWERSHIP FOR A GIVEN PROGRAMME K OF QUALITY X ON SLOT I ON DAY J
 
 # BELOW ALGORITHM TAKEN FROM PAPER http://eprints.lancs.ac.uk/87877/1/2017haniphd.pdf
 # THESIS DETAILS OPTIMISATION FOR TV SCHEDULES BASED ON INCREASING VIEWERSHIP PER PROGRAMME
@@ -28,10 +28,11 @@ programme_types = ['FACTUAL', 'TRUE CRIME', 'REALITY', 'PARANORMAL', 'GAMESHOW/Q
 slots_per_day_count = 96
 grades_of_programme_count = 3
 grade_weightings = [0.2, 0.1, 0.05]
-programme_weightings = [0.5, 0.3, 0.6, 0.7, 0.4]
+programme_weightings = [0.5, 0.3, 0.4, 0.7, 0.4]
 total_amount_of_hours_per_genre = 45
 
 start_index = 1
+
 
 def main():
     solver = pywrapcp.Solver('schedule_programmes_viewership')
@@ -50,12 +51,19 @@ def main():
     optimised_slots = []
     total_viewership_count = 0
 
+    # add constraint to only play genres that are show anytime before slot 64 per day
+    show_anytime_cut_off = 64
+
     for day in days:
         for slot in time_slots:
-            items = [combo for combo in total_combos if combo[0] == day and combo[1] == slot]
-            sorted_items = sorted(items, key=itemgetter(4), reverse=True)
-            total_viewership_count += sorted_items[0][4]
-            optimised_slots.append(sorted_items[0])
+            possible_programme_slots = [combo for combo in total_combos if combo[0] == day and combo[1] == slot]
+
+            if slot <= show_anytime_cut_off:
+                possible_programme_slots = remove_watershed_only_programmes(possible_programme_slots)
+
+            sorted_programmes = sorted(possible_programme_slots, key=itemgetter(4), reverse=True)
+            total_viewership_count += sorted_programmes[0][4]
+            optimised_slots.append(sorted_programmes[0])
 
     for optimised_slot in optimised_slots:
         print('Day: %s ' % calendar.day_abbr[optimised_slot[0] - 1])
@@ -88,7 +96,7 @@ def get_total_contrib_of_viewers(total_possible_combo_count, days, time_slots, g
 def get_contrib_viewer_value(day, slot, programme_index, grade):
     # C(i, k, j, x) = P(i, j, k) * F(x) * POPULATION
 
-    weighting = grade_weightings[grade] #F(x)
+    weighting = grade_weightings[grade]  # F(x)
     population = 100
     percentage_of_viewers = day * slot * (programme_weightings[programme_index])
 
@@ -97,5 +105,21 @@ def get_contrib_viewer_value(day, slot, programme_index, grade):
     return int(viewer_contrib_value)
 
 
+def remove_watershed_only_programmes(possible_programmes_for_slot):
+    anytime_programmes = []
+
+    for programme in possible_programmes_for_slot:
+        programme_type = programme_types[programme[2]]
+        grade = programme[3]
+
+        programme_limit = [programme_limit for programme_limit in programme_grade_limits if
+                           programme_limit.programme_type == programme_type and programme_limit.grade == grade]
+
+        if programme_limit[0].show_anytime is True:
+            anytime_programmes.append(programme)
+
+    return anytime_programmes
+
+
 if __name__ == '__main__':
-  main()
+    main()
